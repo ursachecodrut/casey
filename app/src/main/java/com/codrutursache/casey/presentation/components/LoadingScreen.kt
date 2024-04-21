9d38fd17071f695374f970d156b266fb58cf99a3
Codrut Ursache <ursache.codrut71@gmail.com>
2024-04-21 15:22:54 +0300

    rename loading screen file

--- app/src/main/java/com/codrutursache/casey/presentation/components/LoadingScreen.kt
+++ app/src/main/java/com/codrutursache/casey/presentation/components/LoadingScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
